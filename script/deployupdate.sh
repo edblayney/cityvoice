d4701e6a9d9af053ec8d2dacfe684d8cbbb0270a
Lee Earley <learley@rfx-tech.com>
2016-06-04 10:32:49 -0400

    Added script to set environment variables.

--- script/deployupdate.sh
+++ script/deployupdate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash --login
 cd /var/www/cityvoice/code
-source ~/.bashrc
+source ~/secret_token_env.sh
 rvm use ruby-2.1.7
 bundle install --deployment --without development test
 bundle exec rake assets:precompile db:migrate RAILS_ENV=production
