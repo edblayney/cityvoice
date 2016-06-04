efb0ea7267b437b6a68c31f0944e71869693dbd2
Lee Earley <lee.earley@gmail.com>
2016-06-04 00:12:43 -0400

    Attempt to fix bundle issue.

--- script/deployupdate.sh
+++ script/deployupdate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash --login
-rvm use ruby-2.1.7
 cd /var/www/cityvoice/code
+rvm use ruby-2.1.7
 bundle install --deployment --without development test
 bundle exec rake assets:precompile db:migrate RAILS_ENV=production
 bundle exec rake import:locations
