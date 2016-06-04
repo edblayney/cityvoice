21d4fe0a3cb55346aebd5481a2ed32288cbe3281
Lee Earley <lee.earley@gmail.com>
2016-06-03 22:08:53 -0400

    Added appspec.yml and scripts for AWS CodeDeploy.

---
+++ script/deployupdate.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+rvm use ruby-2.1.7
+bundle install --deployment --without development test
+bundle exec rake assets:precompile db:migrate RAILS_ENV=production
+bundle exec rake import:locations
+bundle exec rake import:questions
