1a44604c0a4dfa69e85d7547512374931d1fdf6f
Michael Schnuerle <michael@dhcp013234.code-louisville-wifi.lmcdc.local>
2016-06-04 13:14:37 -0400

    Changed to live Twilio data

--- app/models/send-sms.rb
+++ app/models/send-sms.rb
@@ -1,10 +1,17 @@
 require 'twilio-ruby'
 
-account_sid = "AC24bd9521e73195d4c0cb1071302048c3"
-auth_token = "68537864cf242067bf8fdf52f2fe9978"
+# free test auth
+#account_sid = "AC24bd9521e73195d4c0cb1071302048c3" 
+#auth_token = "68537864cf242067bf8fdf52f2fe9978"
+
+# live paid auth
+account_sid = "ACd6d6a314312ca8be844902aff86ff066" 
+auth_token = "217ac0433bc7730c43467c3ed033d5ce"
+
 client = Twilio::REST::Client.new account_sid, auth_token
- 
-from = "+15005550006" # Your Twilio number
+
+#from = "+15005550006" # Your TEST Twilio number
+from = "+18448005227 " # Your LIVE Twilio number
  
 friends = {
 "+14153334444" => "Curious George",
