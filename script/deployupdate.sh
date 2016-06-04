6b165571b7d76c6f6d0a295edfdfaea3cbccfd9b
Lee Earley <lee.earley@gmail.com>
2016-06-03 23:53:25 -0400

    Updated deployupdate.sh

--- script/deployupdate.sh
+++ script/deployupdate.sh
@@ -1,6 +1,7 @@
 #!/bin/bash --login
 rvm use ruby-2.1.7
 env
+pwd
 bundle install --deployment --without development test
 bundle exec rake assets:precompile db:migrate RAILS_ENV=production
 bundle exec rake import:locations
