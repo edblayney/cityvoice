c9465f31e3e326ed36f21ca001b8c22a6745e5c4
Lee Earley <lee.earley@gmail.com>
2016-06-04 00:38:38 -0400

    Another try at fixing script.

--- script/deployupdate.sh
+++ script/deployupdate.sh
@@ -1,5 +1,6 @@
 #!/bin/bash --login
 cd /var/www/cityvoice/code
+source ~/.bashrc
 rvm use ruby-2.1.7
 bundle install --deployment --without development test
 bundle exec rake assets:precompile db:migrate RAILS_ENV=production
