3aa25b5b5122938fd630ebcdcca7354b6510515e
Michael Schnuerle <github@purplesquirrel.net>
2016-06-09 14:20:52 -0400

    Style updates

--- config/initializers/leaflet.rb
+++ config/initializers/leaflet.rb
@@ -1,3 +1,3 @@
-Leaflet.tile_layer = 'http://{s}.tile.osm.org/{z}/{x}/{y}.png'
-Leaflet.attribution = '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+Leaflet.tile_layer = 'http://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png'
+Leaflet.attribution = 'Imagery from <a href="http://mapbox.com/about/maps/">MapBox</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 Leaflet.max_zoom = 12
