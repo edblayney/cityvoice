db92ec5f4bd88d17b63a29f53e4fc8a5c152a3e9
Lee Earley <lee.earley@gmail.com>
2016-06-08 22:23:10 -0400

    Added rails production environment variable to other rake tasks.

--- script/deployupdate.sh
+++ script/deployupdate.sh
@@ -4,6 +4,6 @@ source ~/secret_token_env.sh
 rvm use ruby-2.1.7
 bundle install --deployment --without development test
 bundle exec rake tmp:cache:clear
-bundle exec rake assets:precompile db:migrate RAILS_ENV=production
-bundle exec rake import:locations
-bundle exec rake import:questions
+bundle exec rake assets:precompile db:migrate RAILS_ENV="production"
+bundle exec rake import:locations RAILS_ENV="production"
+bundle exec rake import:questions RAILS_ENV="production"
