b54fdc37e8a955ac7b79fdbdf86bc023e1de154a
Lee Earley <lee.earley@gmail.com>
2016-08-28 19:52:17 -0400

    Removed database import tasks from deploy script.

--- script/deployupdate.sh
+++ script/deployupdate.sh
@@ -5,5 +5,3 @@ rvm use ruby-2.1.7
 bundle install --deployment --without development test
 bundle exec rake tmp:cache:clear
 bundle exec rake assets:precompile db:migrate RAILS_ENV="production"
-bundle exec rake import:locations RAILS_ENV="production"
-bundle exec rake import:questions RAILS_ENV="production"
